23bf4e22b7ba62a5b5f382ed97b5e283f8c46da0
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-09 23:51:04 +0600

    data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,42 @@ function App() {
   const [users, setUser] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/user")
+    fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data) => setUser(data));
   }, []);
+
+  const handelAddUser = (event) => {
+    event.preventDefault();
+    const from = event.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("inside post response", data);
+      });
+  };
 
   return (
     <>
       <h1>User Management system</h1>
       <h3>Numbers of users: {users.length}</h3>
+      <form onSubmit={handelAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
       <div>
         {users.map((user) => (
           <p key={user.id}>
